75f5beef17189aa86a83ec4a0705818f06104439
SaqiBoppoTech <saqlain.shaikh@boppotechnologies.com>
2023-11-03 17:20:41 +0530

    'Change'

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import MainStack from './src/navigation/MainStack';
+import BoxSvg from './src/assets/svgs/BoxSvg.svg'
+import { Constant } from './src/global';
 const App = () => {
   return (
     <View style={styles.container}>
