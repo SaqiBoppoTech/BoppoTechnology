add584eff37c8c43cff75333e57496d6cee94cb9
SaqiBoppoTech <saqlain.shaikh@boppotechnologies.com>
2023-11-13 18:41:57 +0530

    'Change'

--- src/components/QuestionAndAnswer/QuestionAndAnswerStyle.js
+++ src/components/QuestionAndAnswer/QuestionAndAnswerStyle.js
@@ -18,49 +18,47 @@ export const styles = StyleSheet.create({
         borderRadius: CHANGE_BY_MOBILE_DPI(7),
         backgroundColor: Colors.GREY_SHADE_500,
         marginHorizontal: CHANGE_BY_MOBILE_DPI(20),
-        marginTop:CHANGE_BY_MOBILE_DPI(20),
-        paddingHorizontal:CHANGE_BY_MOBILE_DPI(10),
-        paddingVertical:CHANGE_BY_MOBILE_DPI(0)
+        marginTop: CHANGE_BY_MOBILE_DPI(20),
+        paddingHorizontal: CHANGE_BY_MOBILE_DPI(10),
+        paddingVertical: CHANGE_BY_MOBILE_DPI(0)
     },
-    inputFontStyle:{
-        fontSize:CHANGE_BY_MOBILE_DPI(12),
-        fontFamily:Fonts.MANROPE_REGULAR,
-        color:Colors.GRAY_DARK,
+    inputFontStyle: {
+        fontSize: CHANGE_BY_MOBILE_DPI(12),
+        fontFamily: Fonts.MANROPE_REGULAR,
+        color: Colors.GRAY_DARK,
     },
-    positionInputContainer:{
-        position:'absolute',
-        left:CHANGE_BY_MOBILE_DPI(10),
-        right:0
+    positionInputContainer: {
+        position: 'absolute',
+        left: CHANGE_BY_MOBILE_DPI(10),
+        right: 0
     },
-    sendButton:{
-        height:CHANGE_BY_MOBILE_DPI(31),
-        width:CHANGE_BY_MOBILE_DPI(88.40),
-        borderRadius:CHANGE_BY_MOBILE_DPI(3),
-        backgroundColor:Colors.DARK_GREEN,
-        alignItems:'center',
-        justifyContent:'center'
+    sendButton: {
+        height: CHANGE_BY_MOBILE_DPI(31),
+        width: CHANGE_BY_MOBILE_DPI(88.40),
+        borderRadius: CHANGE_BY_MOBILE_DPI(3),
+        backgroundColor: Colors.DARK_GREEN,
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    sendFontStyle:{
-        fontSize:CHANGE_BY_MOBILE_DPI(14),
-        fontFamily:Fonts.MANROPE_BOLD,
-        color:Colors.WHITE,
+    sendFontStyle: {
+        fontSize: CHANGE_BY_MOBILE_DPI(14),
+        fontFamily: Fonts.MANROPE_BOLD,
+        color: Colors.WHITE,
     },
-    sendPositionContainer:{
-        marginTop:CHANGE_BY_MOBILE_DPI(10),
-        marginHorizontal:CHANGE_BY_MOBILE_DPI(20),
-        alignItems:'flex-end'
+    sendPositionContainer: {
+        marginTop: CHANGE_BY_MOBILE_DPI(10),
+        marginHorizontal: CHANGE_BY_MOBILE_DPI(20),
+        alignItems: 'flex-end'
     },
-    seq:{
-        fontSize:CHANGE_BY_MOBILE_DPI()
+    seq: {
+        fontSize: CHANGE_BY_MOBILE_DPI()
     },
     container: {
-        backgroundColor: Colors.LIGHT_WHITE,
-        margin: CHANGE_BY_MOBILE_DPI(10),
-        borderRadius: CHANGE_BY_MOBILE_DPI(10),
-        paddingTop: CHANGE_BY_MOBILE_DPI(5),
-      },
-    
-      bottomContainer: {
+        backgroundColor: Colors.WHITE,
+
+    },
+
+    bottomContainer: {
         flexDirection: 'row',
         height: CHANGE_BY_MOBILE_DPI(45),
         borderBottomRightRadius: CHANGE_BY_MOBILE_DPI(10),
@@ -68,37 +66,58 @@ export const styles = StyleSheet.create({
         backgroundColor: Colors.LIGHT_WHITE,
         alignItems: 'center',
         justifyContent: 'center',
-      },
-    
-      optionText: {
+    },
+
+
+    optionText: {
         fontSize: CHANGE_BY_MOBILE_DPI(14),
         color: Colors.BLACK,
         fontFamily: Fonts.MANROPE_BOLD,
-      },
-      line: {
+    },
+    line: {
         borderBottomWidth: CHANGE_BY_MOBILE_DPI(1),
         borderColor: Colors.GAINSBORO,
         marginTop: CHANGE_BY_MOBILE_DPI(10),
-      },
-    
-      question: {
+    },
+
+    question: {
         fontFamily: Fonts.MANROPE_BOLD,
         fontSize: CHANGE_BY_MOBILE_DPI(14),
         color: Colors.BLACK_DARK,
-      },
-      questionView: {
+    },
+    questionView: {
         flexDirection: 'row',
         flex: 1,
         marginHorizontal: CHANGE_BY_MOBILE_DPI(10),
         marginLeft: CHANGE_BY_MOBILE_DPI(20),
-      },
-      questionText: {
+    },
+    questionText: {
         fontFamily: Fonts.MANROPE_MEDIUM,
         fontSize: CHANGE_BY_MOBILE_DPI(16),
         color: Colors.BLACK_DARK,
         marginHorizontal: CHANGE_BY_MOBILE_DPI(5),
-      },
-      flexView: {
+    },
+    flexView: {
         flex: 1,
-      },
+    },
+    flexDirection: {
+        flexDirection: 'row',
+        marginHorizontal:CHANGE_BY_MOBILE_DPI(20)
+    },
+    qustionFontStyle: {
+        fontSize: CHANGE_BY_MOBILE_DPI(14),
+        fontFamily: Fonts.MANROPE_BOLD,
+        color: Colors.BLACK
+    },
+    lessFlexContainer: {
+        flex: 0.2
+    },
+    largeFlexContainer: {
+        flex: 0.7
+    },
+    valueFontStyle: {
+        fontSize: CHANGE_BY_MOBILE_DPI(14),
+        fontFamily: Fonts.MANROPE_REGULAR,
+        color: Colors.BLACK
+    }
 });
